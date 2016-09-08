fad24135a4eadbecb7a98987e132abbdc3c3f192
cihrsliu <liuc@d-e.org>
2016-09-07 21:55:37 -0400

    viewer and mandelbrot

--- test.py
+++ test.py
@@ -1,24 +1,17 @@
 from tkg import *
+import timeit
+
+class TestViewer(Viewer):
+    def draw(self):
+        setup = '''
+import tkinter as tk
+master = tk.Tk()
+canvas = tk.Canvas(master)
+canvas.pack()
+'''
+        print(timeit.timeit(setup=setup, stmt='canvas.create_rectangle(10, 10, 10, 10, fill="white", outline="")', number=600*600))
+        print('done')
 
 
-def log(message):
-    print(message)
-
-
-class Viewer(Page):
-    def load(self):
-        self.canvas = tk.Canvas(self, width=600, height=600)
-        self.canvas.pack()
-        self.canvas.create_rectangle(0, 0, 600, 600, fill='white')
-
-        upb = tk.Button(self, text='Up', width=4, height=2, command=lambda: log('Up'))
-        upb.place(x=100, y=650)
-        downb = tk.Button(self, text='Down', width=4, height=2, command=lambda: log('Down'))
-        downb.place(x=100, y=690)
-        leftb = tk.Button(self, text='Left', width=4, height=2, command=lambda: log('Left'))
-        leftb.place(x=60, y=670)
-        leftb = tk.Button(self, text='Right', width=4, height=2, command=lambda: log('Right'))
-        leftb.place(x=140, y=670)
-
-app = Window(title='Viewer', size=(600, 800), frameclasses=(Viewer,))
+app = Window(title='Viewer', size=(600, 800), frameclasses=(TestViewer,))
 app.mainloop()
--- tkg.py
+++ tkg.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 FONT = ('Verdana', 12)
 
 
+def log(message):
+    print(message)
+
+
 class Page(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -13,14 +17,63 @@ class Page(tk.Frame):
         pass
 
 
-class TestPage(Page):
+class Viewer(Page):
     def load(self):
-        label = tk.Label(self, text='Test Page', font=FONT)
+        self.canvas = tk.Canvas(self, width=600, height=600)
+        self.canvas.pack()
+        self.canvas.create_rectangle(0, 0, 600, 600, fill='white')
+
+        upb = tk.Button(self, text='Up', width=4, height=2, command=lambda: self.up())
+        upb.place(x=100, y=650)
+        downb = tk.Button(self, text='Down', width=4, height=2, command=lambda: self.down())
+        downb.place(x=100, y=690)
+        leftb = tk.Button(self, text='Left', width=4, height=2, command=lambda: self.left())
+        leftb.place(x=60, y=670)
+        leftb = tk.Button(self, text='Right', width=4, height=2, command=lambda: self.right())
+        leftb.place(x=140, y=670)
+
+        z1 = tk.Button(self, text='Zoom In', width=6, height=2, command=lambda: self.zoom_in())
+        z1.place(x=200, y=650)
+        z2 = tk.Button(self, text='Zoom Out', width=6, height=2, command=lambda: self.zoom_out())
+        z2.place(x=280, y=650)
+
+        self.setvars()
+        self.draw()
+
+    #will override
+    def setvars(self):
+        pass
+
+    def draw(self):
+        self.canvas.create_rectangle(0, 0, 600, 600, fill='white')
+
+    def up(self):
+        log('Up')
+
+    def down(self):
+        log('Down')
+
+    def left(self):
+        log('Left')
+
+    def right(self):
+        log('Right')
+
+    def zoom_in(self):
+        pass
+
+    def zoom_out(self):
+        pass
+
+
+class TempPage(Page):
+    def load(self):
+        label = tk.Label(self, text='Temp Page', font=FONT)
         label.pack(pady=10, padx=10)
 
 
 class Window(tk.Tk):
-    def __init__(self, title='Title', size=(600,600), frameclasses=(TestPage,), *args, **kwargs):
+    def __init__(self, title='Title', size=(600,600), frameclasses=(TempPage,), *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
         self.wm_title(title)
