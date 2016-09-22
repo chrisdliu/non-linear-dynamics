74478fa97c09df94145e36f5952f757478e1eb53
cihrsliu <liuc@d-e.org>
2016-09-21 23:20:21 -0400

    Add files via upload

--- logistic_graph.py
+++ logistic_graph.py
@@ -3,57 +3,146 @@ from pyg import *
 
 def dynamic(a, x, i, r):
     for n in range(i):
-        x = a*x*(1-x)
+        x = a * x * (1 - x)
     rl = []
     for n in range(r):
-        x = a*x*(1-x)
+        x = a * x * (1 - x)
         rl.append(x)
     return rl
 
 
-class Graph(Window2):
+class GraphScreen(Screen):
+    def up(self):
+        self.sy += self.sh / 5
+        self.render()
+
+    def down(self):
+        self.sy -= self.sh / 5
+        self.render()
+
+    def left(self):
+        self.sx -= self.sw / 5
+        self.render()
+
+    def right(self):
+        self.sx += self.sw / 5
+        self.render()
+
+    def reset(self):
+        if self.mode == 0:
+            self.sx = 3.5
+            self.sy = .5
+            self.sw = 1
+            self.sh = 1
+        elif self.mode == 1:
+            self.sx = 0.5
+            self.sy = 0.5
+            self.sw = 1
+            self.sh = 1
+        self.render()
+
+    def zoomup(self):
+        if self.sz < 1:
+            self.sz += 0.1
+
+    def zoomdown(self):
+        if self.sz > 0:
+            self.sz -= 0.1
+
+    def setmode(self, m):
+        self.mode = m
+        self.reset()
+
     def setvars(self):
-        self.vertex_list = []
-        #zoomed to 3.630315,0.769489 with size 0.000015,0.000015
-        self.x = 3.5
-        self.y = .5
-        self.w = 1
-        self.h = 1
+        # zoomed to 3.630315,0.769489 with size 0.000015,0.000015
+        self.sx = 3.5
+        self.sy = .5
+        self.sw = 1
+        self.sh = 1
+        self.sz = .5
+        self.mode = 0 # 0-bifurcation 1-cobweb
 
     def render(self):
-        for vlist in self.vertex_list:
-            vlist.delete()
-            self.vertex_list.remove(vlist)
+        if self.mode == 0:
+            for px in range(self.w):
+                # a = px * 1 / self.width + 3
+                a = px * self.sw / self.h + self.sx - self.sw / 2
+                orbit = dynamic(a, 0.1, 1000, 200)
+                for py in orbit:
+                    # ppy = py * self.height
+                    ppy = (py - self.sy + self.sh / 2) * self.h / self.sh
+                    if ppy < 0 or ppy > self.h:
+                        continue
+                    self.add_point(px + .5, ppy + .5)
+            print(str(len(self.vertexes['points']) // 3) + ' points')
+        elif self.mode == 1:
+            pass
+        self.flush()
 
-        points = []
-        colors = []
-        for px in range(self.width):
-            #a = px * 1 / self.width + 3
-            a = px * self.w / self.width + self.x - self.w / 2
-            pl = dynamic(a, 0.1, 1000, 200)
-            for py in pl:
-                #ppy = py * self.height
-                ppy = (py - self.y + self.h / 2) * self.height / self.h
-                if ppy < 0 or ppy > self.height:
-                    continue
-                points.extend((px, ppy))
-                colors.extend((0, 0, 0))
-        self.vertex_list.append(self.batch.add(len(points) // 2, GL_POINTS, None, ('v2f', points),
-                                          ('c3B', colors)))
-        print(str(len(points)) + ' points')
+    def on_resize(self, width, height):
+        self.w = width
+        self.h = height - 200
+        self.setbg(self.bg)
 
-    def mouse_release(self, x, y, button, modifiers):
+    def mouse_up(self, x, y, button, modifiers):
         if button == pyglet.window.mouse.LEFT:
-            self.x = self.x - self.w / 2 + x * self.w / self.width
-            self.y = self.y - self.h / 2 + y * self.h / self.height
-            self.w *= 0.5
-            self.h *= 0.5
+            self.sx = self.sx - self.sw / 2 + x * self.sw / self.w
+            self.sy = self.sy - self.sh / 2 + y * self.sh / self.h
+            self.sw *= self.sz
+            self.sh *= self.sz
         elif button == pyglet.window.mouse.RIGHT:
-            self.x = self.x - self.w / 2 + x * self.w / self.width
-            self.y = self.y - self.h / 2 + y * self.h / self.height
-            self.w *= 2
-            self.h *= 2
-        print('zoomed to %f,%f with size %f,%f' % (self.x, self.y, self.w, self.h))
+            self.sx = self.sx - self.sw / 2 + x * self.sw / self.w
+            self.sy = self.sy - self.sh / 2 + y * self.sh / self.h
+            self.sw /= self.sz
+            self.sh /= self.sz
+        print('zoomed to %f,%f with size %f,%f' % (self.sx, self.sy, self.sw, self.sh))
+        self.render()
 
-window = Graph(width=600, height=600, caption='Logistics Graph', bg=(1, 1, 1, 1), resizable=True)
+
+class GraphWindow(Window2):
+    def setvars(self):
+        self.screens.append(GraphScreen(0, 200, 600, 600))
+
+        upb = Button(75, 145, 40, 40, 'Up', self.screens[0].up)
+        self.buttons.append(upb)
+        downb = Button(75, 100, 40, 40, 'Down', self.screens[0].down)
+        self.buttons.append(downb)
+        leftb = Button(30, 120, 40, 40, 'Left', self.screens[0].left)
+        self.buttons.append(leftb)
+        rightb = Button(120, 120, 40, 40, 'Right', self.screens[0].right)
+        self.buttons.append(rightb)
+        resetb = Button(180, 120, 40, 40, 'Reset', self.screens[0].reset)
+        self.buttons.append(resetb)
+
+        zoomlabel = pyglet.text.Label('Zoom: %.1f' % self.screens[0].sz, font_name='Verdana', font_size=8, x=30, y=50)
+        self.labels.append(zoomlabel)
+        zupb = Button(90, 50, 40, 40, 'Zoom+', self.zoomup)
+        self.buttons.append(zupb)
+        zdownb = Button(140, 50, 40, 40, 'Zoom-', self.zoomdown)
+        self.buttons.append(zdownb)
+
+        m1b = Button(300, 150, 100, 40, 'Bifurcation Graph', lambda: self.screens[0].setmode(0))
+        self.buttons.append(m1b)
+        m2b = Button(300, 100, 100, 40, 'Cobweb Diagram', lambda: self.screens[0].setmode(1))
+        self.buttons.append(m2b)
+
+    def zoomup(self):
+        self.screens[0].zoomup()
+        self.redraw_labels()
+
+    def zoomdown(self):
+        self.screens[0].zoomdown()
+        self.redraw_labels()
+
+    def redraw_labels(self):
+        for label in self.labels:
+            label.delete()
+            self.labels.remove(label)
+        zoomlabel = pyglet.text.Label('Zoom: %.1f' % self.screens[0].sz, font_name='Verdana', font_size=8, x=30, y=50)
+        self.labels.append(zoomlabel)
+
+        self.on_draw()
+
+
+window = GraphWindow(width=600, height=800, caption='Logistics Graph', bg=(0, 0, 0, 1), resizable=True)
 pyglet.app.run()
--- pyg.py
+++ pyg.py
@@ -1,30 +1,6 @@
 from pyglet.gl import *
 
 
-class Button():
-    def __init__(self, x, y, w, h, text, action):
-        self.x = x
-        self.y = y
-        self.w = w
-        self.h = h
-        self.text = text
-        self.action = action
-        self.label = pyglet.text.Label(text, font_name='Verdana', font_size=8, x=x, y=y)
-
-    def click(self, *args, **kwargs):
-        if self.action:
-            self.action(*args, **kwargs)
-
-    def is_inside(self, x, y):
-        return x >= self.x and x < self.x + self.w and y >= self.y and y < self.y + self.h
-
-    def load(self, batch):
-        batch.add(4, GL_QUADS, None, ('v2f', (self.x, self.y, self.x + self.w, self.y, self.x + self.w, self.y + self.h,self.x, self.y + self.h)), ('c3B', (100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100)))
-
-    def draw(self):
-        self.label.draw()
-
-
 class Window(pyglet.window.Window):
     def __init__(self, bg=(0, 0, 0, 1), *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -51,7 +27,7 @@ class Window(pyglet.window.Window):
     def on_mouse_release(self, x, y, button, modifiers):
         self.mouse_release(x, y, button, modifiers)
 
-    #to be overriden
+    # to be overriden
     def setvars(self):
         pass
 
@@ -70,31 +46,161 @@ class Window(pyglet.window.Window):
             if button.is_inside(x, y):
                 button.click()
 
-class Window2(pyglet.window.Window):
-    def __init__(self, caption='Window', bg=(0, 0, 0, 1), *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.set_minimum_size(kwargs['width'], kwargs['height'])
-        glClearColor(bg[0], bg[1], bg[2], bg[3])
+
+class Button:
+    def __init__(self, x, y, w, h, text, action=None):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.text = text
+        self.action = action
+        self.vertex_list = None
+        self.label = pyglet.text.Label(text, font_name='Verdana', font_size=8, x=x, y=y)
+
+    def mouse_up(self, *args, **kwargs):
+        if self.action:
+            self.action(*args, **kwargs)
+
+    def is_inside(self, x, y):
+        return self.x <= x < self.x + self.w and self.y <= y < self.y + self.h
+
+    def render(self, batch):
+        if self.vertex_list:
+            self.vertex_list.delete()
+        self.vertex_list = batch.add(4, GL_QUADS, None,
+            ('v2f', (self.x, self.y, self.x + self.w, self.y, self.x + self.w, self.y + self.h, self.x, self.y + self.h)),
+            ('c3B', (100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100)))
+
+    def draw_label(self):
+        self.label.draw()
+
+
+class Screen:
+    def __init__(self, x, y, width, height, bg=(255, 255, 255)):
+        self.x = x
+        self.y = y
+        self.w = width
+        self.h = height
+
         self.batch = pyglet.graphics.Batch()
+        self.vertex_lists = {}
+        self.vertexes = {}
+        self.colors = {}
+        self.vertex_types = ('points', 'lines',)
+        for type in self.vertex_types:
+            self.vertex_lists[type] = None
+            self.vertexes[type] = []
+            self.colors[type] = []
+
+        self.bg = bg
+        self.setbg(bg)
         self.setvars()
-        self.render()
 
-    def on_mouse_release(self, x, y, button, modifiers):
-        self.mouse_release(x, y, button, modifiers)
+    def setbg(self, color):
+        if 'bg' not in self.vertex_lists.keys():
+            self.vertex_lists['bg'] = None
+        if self.vertex_lists['bg']:
+            self.vertex_lists['bg'].delete()
+        self.vertex_lists['bg'] = self.batch.add(4, GL_QUADS, pyglet.graphics.OrderedGroup(0), (
+            'v2f', (self.x, self.y, self.x, self.y + self.h, self.x + self.w, self.y + self.h, self.x + self.w, self.y)),
+            ('c3B', color * 4))
 
-    def on_draw(self):
-        print('drawing')
-        self.clear()
-        self.render()
-        #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        self.batch.draw()
+    def add_point(self, x, y, color=(0, 0, 0)):
+        self.vertexes['points'].extend((x + self.x, y + self.y))
+        self.colors['points'].extend(color)
 
-    #to be overriden
+    def add_line(self, x1, y1, x2, y2, color=(0, 0, 0)):
+        self.vertexes['lines'].extend((x1 + self.x, y1 + self.y, x2 + self.x, y2 + self.y))
+        self.colors['lines'].extend(color * 2)
+
+    def flush(self):
+        for type in self.vertex_types:
+            if self.vertex_lists[type]:
+                self.vertex_lists[type].delete()
+                self.vertex_lists[type] = None
+            vlist = None
+            if type == 'points':
+                vlist = self.batch.add(len(self.vertexes[type]) // 2, GL_POINTS, pyglet.graphics.OrderedGroup(1),
+                    ('v2f', self.vertexes[type]), ('c3B', self.colors[type]))
+            elif type == 'lines':
+                vlist = self.batch.add(len(self.vertexes[type]) // 4, GL_LINES, pyglet.graphics.OrderedGroup(1),
+                    ('v2f', self.vertexes[type]), ('c3B', self.colors[type]))
+            self.vertex_lists[type] = vlist
+
+        self.clearbuffer()
+
+    def clearbuffer(self):
+        for type in self.vertex_types:
+            self.vertexes[type] = []
+            self.colors[type] = []
+
+    def is_inside(self, x, y):
+        return 0 <= x - self.x < self.w and 0 <= y - self.y < self.h
+
+    # to be overriden
     def setvars(self):
         pass
 
     def render(self):
         pass
 
-    def mouse_release(self, x, y, button, modifiers):
+    def on_resize(self, width, height):
+        pass
+
+
+class Window2(pyglet.window.Window):
+    def __init__(self, width=600, height=600, caption='Window', bg=(0, 0, 0, 1), *args, **kwargs):
+        super().__init__(width=width, height=height, caption=caption, *args, **kwargs)
+        self.set_minimum_size(width, height)
+        glClearColor(bg[0], bg[1], bg[2], bg[3])
+
+        # batch for the window
+        # no vertex_list for window
+        self.batch = pyglet.graphics.Batch()
+        self.screens = []
+        self.buttons = []
+        self.labels = []
+
+        self.setvars()
+        self.render()
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        self.mouse_up(x, y, button, modifiers)
+
+    def on_draw(self):
+        print('drawing')
+        self.clear()
+        self.batch.draw()
+        for screen in self.screens:
+            screen.batch.draw()
+        for button in self.buttons:
+            button.draw_label()
+        for label in self.labels:
+            label.draw()
+
+    # render should add points, then flush to the batch
+    def render(self):
+        for screen in self.screens:
+            screen.render()
+        for button in self.buttons:
+            button.render(self.batch)
+
+    def on_resize(self, width, height):
+        super().on_resize(width, height)
+        for screen in self.screens:
+            screen.on_resize(width, height)
+        self.render()
+
+    # to be overriden
+    def setvars(self):
+        pass
+
+    def mouse_up(self, x, y, button, modifiers):
+        for screen in self.screens:
+            if screen.is_inside(x, y):
+                screen.mouse_up(x - screen.x, y - screen.y, button, modifiers)
+        for b in self.buttons:
+            if b.is_inside(x, y):
+                b.mouse_up()
         print('click: ' + str(x) + ', ' + str(y))
